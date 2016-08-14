70463a7b7f7505589d566c77a250333012fc54d7
Doug Latornell <djl@douglatornell.ca>
2016-08-14 08:10:14 -0700

    Add Pyramid app to serve Sphinx static pages from this repo.

---
+++ salishsea_site/__init__.py
@@ -0,0 +1,30 @@
+# Copyright 2013-2016 The Salish Sea MEOPAR Contributors
+# and The University of British Columbia
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Pyramid web app that serves the salishsea.eos.ubc.ca site
+"""
+from pyramid.config import Configurator
+
+
+def main(global_config, **settings):
+    """ This function returns a Pyramid WSGI application.
+    """
+    config = Configurator(settings=settings)
+    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route('catchall_static', '/*subpath')
+    config.add_view(
+        'salishsea_site.views.static_page', route_name='catchall_static')
+    config.scan()
+    return config.make_wsgi_app()
