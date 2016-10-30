c9ac3611dcee1a9911d2a5feee0fb8d969536c1a
Doug Latornell <djl@douglatornell.ca>
2016-10-30 12:27:39 -0700

    Organize views into modules corresponding to navbar headings.

--- salishsea_site/views/salishseacast.py
+++ salishsea_site/views/salishseacast.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""salishsea_site views
+"""salishsea_site SalishSeaCast views
 """
 import logging
 import os
@@ -21,15 +21,10 @@ from pathlib import Path
 
 import arrow
 from pyramid.httpexceptions import HTTPNotFound
-from pyramid.static import static_view
 from pyramid.view import view_config
 
 
 logger = logging.getLogger(__name__)
-
-
-static_page = static_view(
-    '/var/www/html', use_subpath=True, cache_max_age=3600)
 
 
 @view_config(route_name='nowcast.logs', renderer='string')
@@ -59,9 +54,3 @@ def nowcast_publish(request):
         'plot_title': 'Marine and Atmospheric Conditions - Storm Surge Alerts',
         'svg_file': 'Threshold_website',
     }
-
-
-@view_config(route_name='about.contributors', renderer='contributors.mako')
-@view_config(route_name='about.contributors.html', renderer='contributors.mako')
-def contributors(request):
-    return {}
