483f60c9cfe26a73d4c1fecabf5ee7f2f97e2606
Doug Latornell <djl@douglatornell.ca>
2016-10-31 08:05:21 -0700

    Add Mako filter function to produce slugs for HTML ids and anchor hrefs.

---
+++ tests/test_mako_filters.py
@@ -0,0 +1,35 @@
+# Copyright 2013-2016 The Salish Sea MEOPAR Contributors
+# and The University of British Columbia
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Unit tests for mako_filters module.
+"""
+import pytest
+
+from salishsea_site import mako_filters
+
+
+@pytest.mark.parametrize('text, expected', [
+    ('foo', 'foo'),
+    ('foo bar', 'foo-bar'),
+    ('foo Bar', 'foo-bar'),
+    ('foo-Bar', 'foo-bar'),
+    ('FOO BAR', 'foo-bar'),
+    ('foo - bar', 'foo---bar'),
+    ('foo — bar', 'foo-—-bar'),
+])
+class TestSlug:
+    def test_slug(self, text, expected):
+        slug = mako_filters.slug(text)
+        assert slug == expected
