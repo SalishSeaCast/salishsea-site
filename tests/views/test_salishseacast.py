433b03e4cd86e24bf20d6b99e0c76be815a69f34
Doug Latornell <djl@douglatornell.ca>
2016-11-05 14:40:52 -0700

    Change FigureMetadata.available() method to check server instead of filesystem.

--- tests/views/test_salishseacast.py
+++ tests/views/test_salishseacast.py
@@ -19,14 +19,46 @@
 """
 import os
 from pathlib import Path
-from unittest.mock import patch
+from unittest.mock import (
+    call,
+    Mock,
+    patch,
+)
 
 import arrow
+import pytest
+import requests
 from pyramid.httpexceptions import HTTPNotFound
 from pyramid.threadlocal import get_current_request
-import pytest
 
 from salishsea_site.views import salishseacast
+
+
+@pytest.mark.usefixtures('pconfig')
+@patch('salishsea_site.views.salishseacast.requests.Session')
+class TestFigureMetadata:
+    """Unit tests for FigureMetadata class.
+    """
+
+    def test_available_in_production(self, m_session):
+        request = get_current_request()
+        request.static_url = Mock(name='static_url')
+        figure = salishseacast.FigureMetadata(title='foo', svg_name='bar')
+        figure.available(
+            request, 'nowcast', arrow.get('2016-11-05'), m_session)
+        request.static_url.assert_called_once_with(
+            '/results/nowcast-sys/figures/nowcast/05nov16/bar_05nov16.svg')
+        m_session.head.assert_called_once_with(request.static_url())
+
+    def test_available_in_development(self, m_session):
+        request = get_current_request()
+        request.static_url = Mock(name='static_url')
+        m_session.head.side_effect = (
+            requests.ConnectionError, Mock(status_code=200))
+        figure = salishseacast.FigureMetadata(title='foo', svg_name='bar')
+        figure.available(
+            request, 'nowcast', arrow.get('2016-11-05'), m_session)
+        assert m_session.head.call_args == call(request.static_url().replace())
 
 
 @pytest.mark.usefixtures('pconfig')
@@ -34,6 +66,7 @@ from salishsea_site.views import salishseacast
 class TestNowcastLogs:
     """Unit tests for nowcast_logs view function.
     """
+
     def test_envvar_not_Set(self, m_logger):
         request = get_current_request()
         request.matchdict = {'filename': 'foo'}
@@ -65,13 +98,14 @@ class TestNowcastLogs:
 class TestNowcastPublish:
     """Unit test for nowcast_publish view function.
     """
+
     def test_nowcast_publish(self, m_dfpt):
         request = get_current_request()
         request.matchdict = {'results_date': '04nov16'}
         salishseacast.nowcast_publish(request)
         m_dfpt.assert_called_once_with(
-            'nowcast', arrow.get('2016-11-04'), salishseacast.publish_figures,
-            run_date=arrow.get('2016-11-04'))
+            request, 'nowcast', arrow.get('2016-11-04'),
+            salishseacast.publish_figures, run_date=arrow.get('2016-11-04'))
 
 
 @pytest.mark.usefixtures('pconfig')
@@ -79,13 +113,14 @@ class TestNowcastPublish:
 class TestForecastPublish:
     """Unit test for forecast_publish view function.
     """
+
     def test_forecast_publish(self, m_dfpt):
         request = get_current_request()
         request.matchdict = {'results_date': '04nov16'}
         salishseacast.forecast_publish(request)
         m_dfpt.assert_called_once_with(
-            'forecast', arrow.get('2016-11-04'), salishseacast.publish_figures,
-            arrow.get('2016-11-03'))
+            request, 'forecast', arrow.get('2016-11-04'),
+            salishseacast.publish_figures, arrow.get('2016-11-03'))
 
 
 @pytest.mark.usefixtures('pconfig')
@@ -93,31 +128,37 @@ class TestForecastPublish:
 class TestForecast2Publish:
     """Unit test for forecast2_publish view function.
     """
+
     def test_forecast2_publish(self, m_dfpt):
         request = get_current_request()
         request.matchdict = {'results_date': '04nov16'}
         salishseacast.forecast2_publish(request)
         m_dfpt.assert_called_once_with(
-            'forecast2', arrow.get('2016-11-04'), salishseacast.publish_figures,
-            arrow.get('2016-11-02'))
+            request, 'forecast2', arrow.get('2016-11-04'),
+            salishseacast.publish_figures, arrow.get('2016-11-02'))
 
 
-@patch('salishsea_site.views.salishseacast.FigureMetadata.file_available')
+@pytest.mark.usefixtures('pconfig')
+@patch('salishsea_site.views.salishseacast.FigureMetadata.available')
 class TestDataForPublishTemplate:
     """Unit test for _data_for_publish_template view utility functions.
     """
-    def test_no_alerts_fig_raises_httpnotfound(self, m_file_available):
-        m_file_available.return_value = False
+
+    def test_no_alerts_fig_raises_httpnotfound(self, m_available):
+        request = get_current_request()
+        m_available.return_value = False
         with pytest.raises(HTTPNotFound):
             salishseacast._data_for_publish_template(
-                'nowcast', arrow.get('2016-11-04'),
+                request, 'nowcast', arrow.get('2016-11-04'),
                 salishseacast.publish_figures, arrow.get('2016-11-04'))
 
-    def test_results_date(self, m_file_available):
-        m_file_available.return_value = True
+    def test_results_date(self, m_available):
+        request = get_current_request()
+        m_available.return_value = True
         data = salishseacast._data_for_publish_template(
-            'forecast', arrow.get('2016-11-04'),
-            salishseacast.publish_figures, arrow.get('2016-11-03'))
+            request, 'forecast',
+            arrow.get('2016-11-04'), salishseacast.publish_figures,
+            arrow.get('2016-11-03'))
         assert data['results_date'] == arrow.get('2016-11-04')
 
     @pytest.mark.parametrize('run_type, expected', [
@@ -125,38 +166,48 @@ class TestDataForPublishTemplate:
         ('forecast', 'Forecast'),
         ('forecast2', 'Preliminary Forecast'),
     ])
-    def test_run_type_title(self, m_file_available, run_type, expected):
-        m_file_available.return_value = True
+    def test_run_type_title(self, m_available, run_type, expected):
+        request = get_current_request()
+        m_available.return_value = True
         data = salishseacast._data_for_publish_template(
-            run_type, arrow.get('2016-11-04'),
-            salishseacast.publish_figures, arrow.get('2016-11-04'))
+            request, run_type,
+            arrow.get('2016-11-04'), salishseacast.publish_figures,
+            arrow.get('2016-11-04'))
         assert data['run_type_title'] == expected
 
-    def test_run_type(self, m_file_available):
-        m_file_available.return_value = True
+    def test_run_type(self, m_available):
+        request = get_current_request()
+        m_available.return_value = True
         data = salishseacast._data_for_publish_template(
-            'forecast', arrow.get('2016-11-04'),
-            salishseacast.publish_figures, arrow.get('2016-11-03'))
+            request, 'forecast',
+            arrow.get('2016-11-04'), salishseacast.publish_figures,
+            arrow.get('2016-11-03'))
         assert data['run_type'] == 'forecast'
 
-    def test_run_date(self, m_file_available):
-        m_file_available.return_value = True
+    def test_run_date(self, m_available):
+        request = get_current_request()
+        m_available.return_value = True
         data = salishseacast._data_for_publish_template(
-            'forecast', arrow.get('2016-11-04'),
-            salishseacast.publish_figures, arrow.get('2016-11-03'))
+            request, 'forecast',
+            arrow.get('2016-11-04'), salishseacast.publish_figures,
+            arrow.get('2016-11-03'))
         assert data['run_date'] == arrow.get('2016-11-03')
 
-    def test_figures(self, m_file_available):
-        m_file_available.return_value = True
+    def test_figures(self, m_available):
+        request = get_current_request()
+        m_available.return_value = True
         data = salishseacast._data_for_publish_template(
-            'forecast', arrow.get('2016-11-04'),
-            salishseacast.publish_figures, arrow.get('2016-11-03'))
+            request, 'forecast',
+            arrow.get('2016-11-04'), salishseacast.publish_figures,
+            arrow.get('2016-11-03'))
         assert data['figures'] == salishseacast.publish_figures
 
-    def test_missing_figures(self, m_file_available):
-        m_file_available.side_effect = (
-            [True, True] + [False]*(len(salishseacast.publish_figures)-1))
+    def test_missing_figures(self, m_available):
+        request = get_current_request()
+        m_available.side_effect = (
+            [True, True] + [False] * (len(salishseacast.publish_figures) - 1))
         data = salishseacast._data_for_publish_template(
-            'forecast', arrow.get('2016-11-04'),
-            salishseacast.publish_figures, arrow.get('2016-11-03'))
+            request, 'forecast',
+            arrow.get('2016-11-04'), salishseacast.publish_figures,
+            arrow.get('2016-11-03'))
         assert data['figures'] == [salishseacast.publish_figures[0]]
