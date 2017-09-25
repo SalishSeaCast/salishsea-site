e05e431c2831a3636458b30addb63ab4efdee865
Doug Latornell <djl@douglatornell.ca>
2017-09-24 19:35:48 -0700

    Delete image loop slider mouse interaction code.
    The difficulty of making it instance-specific exceeds its UX value.

--- salishsea_site/static/js/ImageLoop.js
+++ salishsea_site/static/js/ImageLoop.js
@@ -197,10 +197,6 @@ function Slider(container, il, maxIdx) {
       this.items[item].style.left = this.adjustPos(item, pos, 'minux')+'px';
    }
 
-   this.getPos = function(item) {
-      return this.adjustPos(item, this.style2value(this.items[item].style.left), 'plus');
-   }
-
    this.setIndex = function(item, idx) {
       if( idx<this.sliderMinIdx || idx>this.sliderMaxIdx ) {
          return;
@@ -215,117 +211,6 @@ function Slider(container, il, maxIdx) {
       }
    }
 
-   this.pos2idx = function(pos, option) {
-      var idx = pos/this.sliderWidth * (this.sliderMaxIdx-this.sliderMinIdx) + this.sliderMinIdx;
-
-      if( option ) {
-         switch(option) {
-            case 'forceHigh' :
-               idx = Math.round(idx + 0.5);
-               break;
-            case 'forceLow' :
-               idx = Math.round(idx - 0.5);
-               break;
-         }
-      } else {
-         idx = Math.round(idx);
-      }
-
-      return this.fitInto(idx, this.sliderMinIdx, this.sliderMaxIdx);
-   }
-
-   this.getRevisedPos = function(item, pos) {
-      var max = this.getPos('borneMax');
-      var min = this.getPos('borneMin');
-
-      switch(item) {
-         case 'handle' :
-            // Make sure handle is between min and max
-            pos = this.fitInto(pos, min, max);
-            break;
-         case 'borneMin' :
-            // Make sure the min and max are not inversed
-            pos = (pos>max) ? max : pos;
-            break;
-         case 'borneMax' :
-            // Make sure the min and max are not inversed
-            pos = (pos<min) ? min : pos;
-            break;
-      }
-
-      return pos;
-   }
-
-   this.onMouseDown = function(e, item) {
-      il.stop();
-
-      // stop event bubbling
-      if( e.stopPropagation != null ) {
-         e.stopPropagation();
-      } else {
-         e.cancelBubble = true;
-      }
-
-      this.mousedown = 1;
-      this.target = item;
-      // Pos is relative to screenX because it seems to be the only coherent cross-platform coordinate.
-      this.targetInitPos = this.getPos(this.target) - e.screenX;
-   }
-
-   this.onMouseUp = function(e) {
-      if( this.mousedown ) {
-         this.mousedown = 0;
-
-         if( this.target != "" ) {
-            // Find target's revised new position
-            var newPos = this.fitInto(this.targetInitPos+e.screenX, 0, this.sliderWidth);
-            newPos = this.getRevisedPos(this.target, newPos);
-
-            // snap the target back to nearest tick
-            this.setIndex(this.target, this.pos2idx(newPos));
-
-            this.target = "";
-         }
-      }
-   }
-
-   this.onMouseMove = function(e) {
-      if(!e || !this.mousedown || this.target=="" ) {
-         return
-      }
-
-      // find new target position
-      var newPos = this.fitInto(this.targetInitPos+e.screenX, 0, this.sliderWidth);
-      newPos = this.getRevisedPos(this.target, newPos);
-
-      switch(this.target) {
-         case 'handle' :
-            // Adjust animation index
-            var idx = this.pos2idx(newPos);
-            if( idx != this.idxs['handle'] ) {
-               il.setIndex(idx);
-               this.idxs['handle'] = idx;
-            }
-            break;
-         case 'borneMin' :
-            // Make the handle follow (handle can't be left of borneMin)
-            var handle = this.getPos('handle');
-            if( handle < newPos ) {
-               this.setIndex('handle', this.pos2idx(newPos, 'forceHigh'));
-            }
-            break;
-         case 'borneMax' :
-            // Make the handle follow (handle can't be left of borneMin)
-            var handle = this.getPos('handle');
-            if( handle > newPos ) {
-               this.setIndex('handle', this.pos2idx(newPos, 'forceLow'));
-            }
-            break;
-      }
-
-      this.setPos(this.target, newPos);
-   }
-   
    this.init = function(container) {
       // Define dimensions of the slider
 
@@ -379,15 +264,6 @@ function Slider(container, il, maxIdx) {
       borneMax.style.position          = 'absolute';
       borneMax.style.top               = this.handleBorder+'px';
       borneMax.style.backgroundColor   = '#000000';
-
-      // bind events to components
-      var mysl = this;
-      handle.onmousedown = function(e) { e=e||event; mysl.onMouseDown(e, 'handle'); };
-      borneMin.onmousedown = function(e) { e=e||event; mysl.onMouseDown(e, 'borneMin'); };
-      borneMax.onmousedown = function(e) { e=e||event; mysl.onMouseDown(e, 'borneMax'); };
-      var body = document.getElementsByTagName('body')[0];
-      body.onmousemove = function(e) { e=e||event; mysl.onMouseMove(e); };
-      body.onmouseup = function(e) { e=e||event; mysl.onMouseUp(e); };
 
       // Place structures into 'items'
       this.items['slider']   = slider;
